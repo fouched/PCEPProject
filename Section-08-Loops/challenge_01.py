fbaaa0534db7ee245e29b7b5a22b070663485db2
Fouche du Preez <fouche@limehouse.co.za>
2022-06-15 19:40:55 +0200

    added section-08 challenges

---
+++ Section-08-Loops/challenge_01.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for x in numbers:
+    # skip 3 and 7
+    if x == 3 or x == 7:
+        continue
+    print(x)
+
---
+++ Section-08-Loops/challenge_02.py
@@ -0,0 +1,8 @@
+number = 5
+
+while number >= 0:
+    if number > 0:
+        print(number)
+    else:
+        print('GO!')
+    number -= 1
